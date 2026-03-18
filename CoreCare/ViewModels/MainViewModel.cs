dc706757ecc9e11fd209e89b7e543c333ac1a141
amf162-ua <amf162@alu.ua.es>
2026-03-18 18:13:12 +0100

    monitorizacion de procesos y uso de cpu

--- CoreCare/ViewModels/MainViewModel.cs
+++ CoreCare/ViewModels/MainViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CoreCare.Models;
 using CoreCare.Services;
+using System.Collections.ObjectModel;
 using System.Windows.Threading;
 
 namespace CoreCare.ViewModels
@@ -7,20 +9,42 @@ namespace CoreCare.ViewModels
     public partial class MainViewModel : ObservableObject
     {
         private readonly HardwareMonitorService _hardwareService;
+        private readonly ProcessService _processService;
 
         [ObservableProperty]
         private string _cpuDisplay;
 
+        // Esta lista es la que "leerá" el DataGrid en el XAML
+        public ObservableCollection<ProcessItem> Processes { get; set; } = new();
+
         public MainViewModel()
         {
             _hardwareService = new HardwareMonitorService();
+            _processService = new ProcessService();
 
-            // Un temporizador para actualizar la CPU cada segundo
-            var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
-            timer.Tick += (s, e) => {
-                CpuDisplay = _hardwareService.GetCpuLoad();
-            };
+            // Actualizamos cada 2 segundos para no estresar el PC
+            var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(2) };
+            timer.Tick += (s, e) => UpdateAllData();
             timer.Start();
+
+            UpdateAllData(); // Primera carga al abrir
+        }
+
+        private void UpdateAllData()
+        {
+            // 1. Actualizamos el texto de la CPU
+            CpuDisplay = _hardwareService.GetCpuLoad();
+
+            // 2. Obtenemos los procesos del servicio
+            var list = _processService.GetActiveProcesses();
+
+            // 3. Refrescamos la colección de la pantalla
+            Processes.Clear();
+            foreach (var item in list)
+            {
+                Processes.Add(item);
+            }
         }
     }
 }
+
