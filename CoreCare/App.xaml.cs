e0a6c72eb8c1bf705a18be9087b80f49fbf43802
maha <me58@alu.ua.es>
2026-03-18 22:39:19 +0100

    configurado la base de datos SQLite con migracion para crear la tabla RegistrosBenchmark

--- CoreCare/App.xaml.cs
+++ CoreCare/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using CoreCare.Data;
 
 namespace CoreCare
 {
@@ -9,6 +10,19 @@ namespace CoreCare
     /// </summary>
     public partial class App : Application
     {
+        // Comprueba si la base de datos existe y la crea si no existe,
+        // así todos obtienemos la BD automáticamente
+        // la primera vez que ejecutamos el proyecto. 
+        // por ahora la db solo contiene la tabla de registros de resultados del benchmark
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // Asegurarse de que la base de datos se crea al iniciar la aplicación
+            using (var db = new CoreCareDbContext())
+            {
+                db.Database.EnsureCreated();
+            }
+        }
     }
 
 }
---
+++ CoreCare/Data/CoreCareDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CoreCare.Models;
+using Microsoft.EntityFrameworkCore;
+using Mono.Unix;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace CoreCare.Data
+{
+    // por ahora solo contiene la tabla donde se guarda los resultados del benchark,
+    // luego se añaden las cosas de IA y usuarios cuando se implementan
+    public class CoreCareDbContext : DbContext
+    {
+        public DbSet<RegistroBenchmark> RegistrosBenchmark { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=corecare.db");
+
+    }
+}
