3130ba59dac7ce66ec4db0e2a8b72d078b67c172
jpv49-ua <124249210+jpv49-ua@users.noreply.github.com>
2026-04-14 12:38:38 +0100

    Clase datos del ordenador

---
+++ CoreCare/Models/SystemTelemetryMock.cs
@@ -0,0 +1,25 @@
+using System.Text.Json.Serialization;
+
+namespace CoreCare.Models
+{
+    public class SystemTelemetryMock
+    {
+        [JsonPropertyName("cpu_usage_percent")]
+        public double CpuUsagePercent { get; set; }
+
+        [JsonPropertyName("cpu_temperature_c")]
+        public double CpuTemperatureC { get; set; }
+
+        [JsonPropertyName("ram_total_gb")]
+        public double RamTotalGb { get; set; }
+
+        [JsonPropertyName("ram_used_gb")]
+        public double RamUsedGb { get; set; }
+
+        [JsonPropertyName("disk_type")]
+        public string DiskType { get; set; } = string.Empty; // Ej: "HDD" o "SSD"
+
+        [JsonPropertyName("disk_usage_percent")]
+        public double DiskUsagePercent { get; set; }
+    }
+}
