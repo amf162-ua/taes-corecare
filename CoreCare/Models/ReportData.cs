6c8d1db304dae894b482f93e286fdef8be35218c
jpv49-ua <124249210+jpv49-ua@users.noreply.github.com>
2026-04-17 12:57:52 +0100

    Solución de comentarios en ReportData

--- CoreCare/Models/ReportData.cs
+++ CoreCare/Models/ReportData.cs
@@ -10,10 +10,10 @@ namespace CoreCare.Models
         public string ClientName { get; set; } = string.Empty;
         public DateTime ReportDate { get; set; }
 
-        // Datos del sistema analizado (reutilizamos el que creaste con tu compañero)
+        // Datos del sistema analizado
         public SystemTelemetryMock TelemetryData { get; set; } = new SystemTelemetryMock();
 
-        // Recomendaciones (hasta que tu compañero haga el Parser, lo simularemos con strings)
+        // Recomendaciones (hasta este el Parser, lo simularemos con strings)
         public List<string> Recommendations { get; set; } = new List<string>();
     }
 }
